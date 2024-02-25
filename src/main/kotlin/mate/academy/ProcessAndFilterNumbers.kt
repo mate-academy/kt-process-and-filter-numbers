2a63a96485f4adab1c642cf6b148b415f56390cc
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2024-02-25 17:48:35 +0200

    Fixed magis numbers.

--- src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
+++ src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
@@ -1,7 +1,13 @@
 package mate.academy
 
+private const val MORE_THEN_VALUE = 25
+
+private const val TWO = 2
+
+private const val ZERO = 0
+
 fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
     return numbers.toMutableList()
-        .map {if (it % 2 == 0) it / 2 else it * 2}
-        .filter{it > 25}
+        .map {if (it % TWO == ZERO) it / TWO else it * TWO }
+        .filter{it > MORE_THEN_VALUE }
 }
