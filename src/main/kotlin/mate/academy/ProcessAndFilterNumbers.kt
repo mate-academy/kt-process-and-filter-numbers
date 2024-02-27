a8366aaced0aae75760d73ea30b8597cd80fb21d
igork <igorkorolj@gmail.com>
2024-02-27 20:09:32 +0200

    implemented

--- src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
+++ src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
@@ -1,9 +1,9 @@
 package mate.academy
 
-const val min = 25
+const val MIN = 25
 fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
     var nums = numbers.map {
         if (it % 2 == 0 ) it / 2 else it * 2
     }
-    return nums.filter { it > min }
+    return nums.filter { it > MIN }
 }
