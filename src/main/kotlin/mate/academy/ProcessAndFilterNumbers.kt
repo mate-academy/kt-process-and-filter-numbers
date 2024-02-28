51577b2824ceb359cbc113db141f7dedbfeddaf5
Nikita <nsalogub96@gmail.com>
2024-02-28 13:38:04 +0200

    fix

--- src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
+++ src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
@@ -1,5 +1,9 @@
 package mate.academy
 
+const val DIVIDER = 2
+const val RESULT_OF_DIVIDING = 0
+const val CONDITION_NUMBER = 25
 fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
-    return numbers.map { if (it % 2 == 0) it / 2 else it * 2 }.filter { it > 25 }
+    return numbers.map { if (it % DIVIDER == RESULT_OF_DIVIDING) it / DIVIDER else it * DIVIDER }
+        .filter { it > CONDITION_NUMBER }
 }
