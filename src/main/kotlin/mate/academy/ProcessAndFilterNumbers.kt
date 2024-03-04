23ce2a519afc5d9758372bfec9c200f008807be5
Valerii Fedorovych <valera.fedorovich.2000@gmail.com>
2024-03-04 16:18:33 +0200

    added constants

--- src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
+++ src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
@@ -1,5 +1,9 @@
 package mate.academy
 
+const val TWO_VALUE = 2
+const val ZERO_VALUE = 0
+const val FILTER_VALUE = 25
+
 fun processAndFilterNumbers(numbers: List<Int>) = numbers
-    .map { if (it % 2 == 0) it / 2 else it * 2 }
-    .filter { it > 25 }
+        .map { if (it % TWO_VALUE == ZERO_VALUE) it / TWO_VALUE else it * TWO_VALUE }
+        .filter { it > FILTER_VALUE }
