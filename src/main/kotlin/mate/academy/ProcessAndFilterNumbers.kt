fb483cc36b8783d187e02b90ca9a6c67024904c3
Svyatoslav Tsytsyura <svatik.bos@gmail.com>
2024-03-06 00:16:45 +0200

    done

--- src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
+++ src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
@@ -1,5 +1,16 @@
 package mate.academy
 
+const val CONSTANT = 25
 fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
+    val list: MutableList<Int> = mutableListOf()
 
+    for (number in numbers) {
+        if (number % 2 == 0 && number / 2 > CONSTANT) {
+            list.add(number / 2)
+        } else if (number % 2 != 0 && number * 2 > CONSTANT) {
+            list.add(number * 2)
+        }
+    }
+
+    return list
 }
