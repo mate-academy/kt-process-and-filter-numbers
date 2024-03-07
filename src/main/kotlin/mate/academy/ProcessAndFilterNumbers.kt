0f758fd9994d10e91ef2cb4cd92dadbf1b445932
Danil Ponomarenko <danil.ponomvrenko@gmail.com>
2024-03-07 16:22:35 +0200

    Practiced chain call from collection type (map, filter)

--- src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
+++ src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
@@ -1,5 +1,7 @@
 package mate.academy
 
 fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
-
+    return numbers.map {
+        n -> if (n % 2 == 0) n / 2 else n * 2
+    }.filter { it > 25 }
 }
