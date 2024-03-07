5b539c1a51472ae773bb5a3f16183765d9ce68de
Danil Ponomarenko <danil.ponomvrenko@gmail.com>
2024-03-07 16:42:45 +0200

    Avoided magic numbers by making a constant

--- src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
+++ src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
@@ -1,7 +1,9 @@
 package mate.academy
 
+const val TWENTY_FIVE = 25
+
 fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
     return numbers.map {
         n -> if (n % 2 == 0) n / 2 else n * 2
-    }.filter { it > 25 }
+    }.filter { it > TWENTY_FIVE }
 }
