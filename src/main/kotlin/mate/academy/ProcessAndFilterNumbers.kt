e193e6c3a5fd97d161524ac0d18ceb462305ccf8
Alex-zzzz <141009519+Alex-zzzz@users.noreply.github.com>
2024-03-14 19:27:05 +0200

    second version

--- src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
+++ src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
@@ -1,13 +1,13 @@
 package mate.academy
-
+const val MAGIC_NUMBER = 25
 fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
     val answer: MutableList<Int> = mutableListOf()
     var index = 0
     for (i in numbers.indices) {
-        if (numbers[i] % 2 == 0 && (numbers[i] / 2) > 25) {
+        if (numbers[i] % 2 == 0 && (numbers[i] / 2) > MAGIC_NUMBER) {
             answer.add(index++,numbers[i] / 2)
         }
-        if (numbers[i] % 2 != 0 && (numbers[i] * 2) > 25) {
+        if (numbers[i] % 2 != 0 && (numbers[i] * 2) > MAGIC_NUMBER) {
             answer.add(index++, numbers[i] * 2)
         }
     }
