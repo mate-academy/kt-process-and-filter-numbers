32a8fcda9a71632f19bf550da1b79a391c14c821
Alona <alenavyrodova@gmail.com>
2024-03-25 13:26:45 +0200

    work with map and filter

--- src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
+++ src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
@@ -1,5 +1,14 @@
 package mate.academy
 
 fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
-
+    val filteredList = numbers.map {
+        val result = if (it % 2 == 0) {
+            it / 2
+        } else {
+            it * 2
+        }
+        result
+    }.filter { it > 25 }
+        .toList()
+    return (filteredList)
 }
