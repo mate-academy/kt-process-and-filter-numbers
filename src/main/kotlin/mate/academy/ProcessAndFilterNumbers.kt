fda70b43346d1f2622c87b4f3c4b10ce54785acf
Denys Okhrymovych <denys.ohrymovych.2015@gmail.com>
2024-03-25 22:57:16 +0200

    remove magic number

--- src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
+++ src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
@@ -1,5 +1,7 @@
 package mate.academy
 
+const val MINIMUM = 26
+
 fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
-    return numbers.map { if (it % 2 == 0) it / 2 else it * 2 }.filter { it > 25 }
+    return numbers.map { if (it % 2 == 0) it / 2 else it * 2 }.filter { it >= MINIMUM }
 }
