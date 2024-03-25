ad2eb35342d0e451276050115cb0aef9b50694ff
Alona <alenavyrodova@gmail.com>
2024-03-25 16:16:30 +0200

    replace magic number

--- src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
+++ src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
@@ -1,14 +1,19 @@
 package mate.academy
 
+const val NUMBER_MORE = 25
+const val DOUBLE_OR_MULTIPLY = 2
 fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
+
     val filteredList = numbers.map {
         val result = if (it % 2 == 0) {
-            it / 2
+            it / DOUBLE_OR_MULTIPLY
         } else {
-            it * 2
+            it * DOUBLE_OR_MULTIPLY
         }
         result
-    }.filter { it > 25 }
+    }.filter { it > NUMBER_MORE }
         .toList()
     return (filteredList)
 }
+
+
