4d9c983abaffb46e13125594b3af39753324d76c
AntonBabychP1T <antonbabi13@gmail.com>
2024-05-25 11:57:57 +0300

    The task is completed

--- src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
+++ src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
@@ -1,5 +1,8 @@
 package mate.academy
 
-fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
+const val VALID_NUMBER = 25
 
+fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
+    return numbers.map {n -> if (n % 2 == 0 ) n / 2 else n * 2 }
+        .filter { it > VALID_NUMBER }
 }
