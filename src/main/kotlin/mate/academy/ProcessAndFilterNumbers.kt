e853416accada3c23905639b931b9e77169905ec
Nonzeq <bgdn.kobylchak@gmail.com>
2024-05-29 17:16:49 +0300

    impl

--- src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
+++ src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
@@ -1,5 +1,7 @@
 package mate.academy
-
+const val NUM = 2;
 fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
-
+    return numbers
+        .map { if (it % NUM != 0) it * NUM else it / NUM}
+        .filter { it > 25 }
 }
