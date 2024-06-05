a3a03da788820f07308b46491324b1d4106ff16d
Dmitri Kolos <skil@bigmir.net>
2024-06-05 23:04:36 +0300

    process and filter numbers solution

--- src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
+++ src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
@@ -1,7 +1,9 @@
 package mate.academy
 
+const val DIVIDER = 2
+const val MIN_VALUE = 25
 fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
    return numbers
-            .map { if (it % 2 == 0) it / 2 else it * 2 }
-            .filter { it > 25 }
+            .map { if (it % DIVIDER == 0) it / DIVIDER else it * DIVIDER }
+            .filter { it > MIN_VALUE }
 }
