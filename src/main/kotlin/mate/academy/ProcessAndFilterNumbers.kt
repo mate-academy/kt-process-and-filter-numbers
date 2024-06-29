f429c05ac5e18cdd16b0d64f9393148af8fb6cff
igortrohimcuk <trokhymchuk.ihor.dev@gmail.com>
2024-06-29 16:57:23 +0300

    Solution

--- src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
+++ src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
@@ -1,5 +1,10 @@
 package mate.academy
 
-fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
+private const val TWENTY_FIVE = 25
 
+fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
+    return numbers.asSequence()
+        .map {if (it % 2 == 0) it /2 else it*2}
+        .filter { it > TWENTY_FIVE }
+        .toList()
 }
