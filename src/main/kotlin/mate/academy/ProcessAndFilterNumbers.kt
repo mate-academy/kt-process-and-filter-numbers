430b8e4a4a0f20d81ded12ad6d811d8d875a608b
Dmytro Hadiuchko <hadiuchko.dmytro@gmail.com>
2024-06-29 12:12:10 +0300

    added hw

--- src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
+++ src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
@@ -1,5 +1,9 @@
 package mate.academy
 
+const val TWO = 2
+const val MAX_VALUE = 25
 fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
-
+    return numbers.map { if (it % 2 == 0) it.div(TWO) else it.times(TWO)  }
+        .filter { it > MAX_VALUE }
+        .toList()
 }
