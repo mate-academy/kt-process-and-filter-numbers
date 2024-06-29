da940f3549a38d8045b7ffbab5431f5acbf3b0d9
Artem Akymenko <akimenko.artem@gmail.com>
2024-06-29 12:15:51 +0300

    Refactored

--- src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
+++ src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
@@ -1,3 +1,6 @@
 package mate.academy
 
-fun processAndFilterNumbers(numbers: List<Int>) = numbers.map { if (it % 2 == 0) it / 2 else it * 2 }.filter { it > 25 }
+private const val MIN_NUM = 25
+
+fun processAndFilterNumbers(numbers: List<Int>)
+= numbers.map { if (it % 2 == 0) it / 2 else it * 2 }.filter { it > MIN_NUM }
