a39a2c9e34aba9d958aac5749681b8953b1efe9f
TonyH277 <gajjduktoxa@gmail.com>
2024-07-04 13:59:20 +0300

    implement processAndFilterNumbers fun

--- src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
+++ src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
@@ -1,5 +1,10 @@
 package mate.academy
 
-fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
+private const val TWO = 2
 
+private const val RANGE = 25
+
+fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
+    return numbers.map { if (it % TWO == 0) it / TWO else it * TWO }
+        .filter { it > RANGE }
 }
