63a838d529a3e35bb70eacc71ca928dc872c2d3e
Ruslan Cherniavskyi <ruslan.od.ua@ukr.net>
2024-08-06 10:40:13 +0300

    added magic number

--- src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
+++ src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
@@ -1,11 +1,15 @@
 package mate.academy
 
+const val DIVISOR = 2
+const val MULTIPLIER = 2
+const val FILTER_THRESHOLD = 25
+
 fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
     return numbers.map { number ->
-        if (number % 2 == 0) {
-            number / 2
+        if (number % DIVISOR == 0) {
+            number / DIVISOR
         } else {
-            number * 2
+            number * MULTIPLIER
         }
-    }.filter { it > 25 }
+    }.filter { it > FILTER_THRESHOLD }
 }
