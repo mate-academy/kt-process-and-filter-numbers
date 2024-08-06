0e33c97937327dd851b6f8111508bb7ab404a245
John <pandastars535@gmail.com>
2024-08-07 02:02:46 +0300

    solution

--- src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
+++ src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
@@ -1,5 +1,15 @@
 package mate.academy
 
 fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
+    var tem = mutableListOf<Int>()
+    for (i in 0 until numbers.size) {
+        if (numbers[i] % 2 == 1) {
+            tem.add(numbers[i] * 2)
+        } else {
+            tem.add(numbers[i] / 2)
+        }
 
+    }
+    var toList = tem.asSequence().filter { it > 25 }.toList()
+    return toList
 }
