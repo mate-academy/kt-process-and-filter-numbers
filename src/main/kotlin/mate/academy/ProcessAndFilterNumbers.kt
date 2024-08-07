51a3a0774aaeee2a1f2c4dc526a0818b55d33aae
Max <mpbakunov@gmail.com>
2024-08-07 19:57:55 +0200

    Bug fixed #02

--- src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
+++ src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
@@ -1,7 +1,7 @@
 package mate.academy
 
-const val filterValue = 25
+const val FILTER_VALUE = 25
 
 fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
-    return numbers.map {if(it % 2 == 0) it / 2 else it * 2}.filter { it > filterValue }
+    return numbers.map {if(it % 2 == 0) it / 2 else it * 2}.filter { it > FILTER_VALUE}
 }
