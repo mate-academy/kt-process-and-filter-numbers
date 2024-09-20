5b57bfc9ae5ed3d120e015f9af04a45c575b933e
Rostyslav Kuzmych <rostislavkuzmic401@gmail.com>
2024-09-20 18:30:55 +0300

    Implemented the feature

--- src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
+++ src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
@@ -1,5 +1,15 @@
 package mate.academy
 
-fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
+const val TWENTY_FIFE = 25
+const val ZERO = 0
+const val TWO = 2
 
+fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
+    val listOfResultIntegers = mutableListOf<Int>()
+
+    for (number in numbers) {
+        listOfResultIntegers.add(if (number % TWO == ZERO) number / TWO else number * TWO)
+    }
+
+    return listOfResultIntegers.filter { it > TWENTY_FIFE }
 }
