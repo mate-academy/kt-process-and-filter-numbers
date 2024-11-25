abfe470dafb2d05d6ecd4226bf557ebce6837229
Anatolii Nazarov <anatoly_n@ukr.net>
2024-11-25 12:11:54 +0200

    For check

--- src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
+++ src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
@@ -1,5 +1,13 @@
 package mate.academy
 
 fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
-
+    val list = mutableListOf<Int>()
+    for (i in numbers) {
+        if (i % 2 == 0) {
+            list.add(i / 2)
+        } else {
+            list.add(i * 2)
+        }
+    }
+    return list.filter { it > 25 }
 }
