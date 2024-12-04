9ec673833271ffecf19bbe7526a656e9e1d46e25
kraievskyi-om <kraievskyi-om@ukrposhta.com>
2024-12-04 12:32:04 +0200

    initial

--- src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
+++ src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
@@ -1,5 +1,6 @@
 package mate.academy
+const val MAGIC = 25
 
-fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
-
-}
+fun processAndFilterNumbers(numbers: List<Int>): List<Int> = numbers
+    .map { if (it % 2 == 0)  it / 2  else it * 2 }
+    .filter { it > MAGIC }
