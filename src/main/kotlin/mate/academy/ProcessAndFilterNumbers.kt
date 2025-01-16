fd2968937a7d1baedf5fa4452784ad5c70de3845
oleksii.b <oleksii.b@dev.tagsoft.co>
2025-01-16 15:35:36 +0200

    refactored MagicNumbers

--- src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
+++ src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
@@ -1,14 +1,19 @@
 package mate.academy
 
+private const val CHECK_VALUE = 25
+private const val ODD_VALUE_CHECK = 2
+private const val INCREMENT_VALUE = 2
+private const val REM_VALUE = 1
+
 fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
     val resultSet: MutableSet<Int> = mutableSetOf()
     numbers.forEach { num ->
-        var value: Int = if (num % 2 == 1) {
-            num * 2
+        var value: Int = if (num % ODD_VALUE_CHECK == REM_VALUE) {
+            num * INCREMENT_VALUE
         } else {
-            num / 2
+            num / INCREMENT_VALUE
         }
-        if (value > 25) {
+        if (value > CHECK_VALUE) {
             resultSet.add(value)
         }
     }
