a5662b5f30598b299d086b9790663820ee68d223
oleksii.b <oleksii.b@dev.tagsoft.co>
2025-01-16 15:27:41 +0200

    resolved task

--- src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
+++ src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
@@ -1,5 +1,17 @@
 package mate.academy
+//You have a `List` of integers. If a number is odd, double it. If a number is even, half it. Return a subset of the collection with numbers greater than `25`.
 
 fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
-
+    val resultSet: MutableSet<Int> = mutableSetOf()
+    numbers.forEach { num ->
+        var value: Int = if (num % 2 == 1) {
+            num * 2
+        } else {
+            num / 2
+        }
+        if (value > 25) {
+            resultSet.add(value)
+        }
+    }
+    return resultSet.toList()
 }
