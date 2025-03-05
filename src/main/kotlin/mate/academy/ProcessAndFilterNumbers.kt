3bf933bfa9de77c683b16f103c15425160a6e7c3
loranazarenko <loranazarenko@ukr.net>
2025-03-05 16:23:56 +0200

    First commit

--- src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
+++ src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
@@ -1,5 +1,16 @@
 package mate.academy
 
+const val CN = 25
 fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
-
+    val seen = mutableSetOf<Int>()
+    numbers.forEach {
+        if (it % 2 == 0) {
+            if (it/2>CN)
+            seen.add(it/2)
+        } else {
+            if (it*2>CN)
+            seen.add(it*2)
+        }
+    }
+    return seen.toList()
 }
