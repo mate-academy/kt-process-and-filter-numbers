35ef99798afb54349a981deeedeb7a6f02ddaa85
Bohdan Lysachenko <lysachenko111@ukr.net>
2025-06-04 16:28:41 +0300

    Solution

--- src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
+++ src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
@@ -1,5 +1,9 @@
 package mate.academy
 
-fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
+const val TWO = 2
+const val ZERO = 0
+const val NUM = 25
 
+fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
+    return numbers.map { if (it % TWO == ZERO) it / TWO else it * TWO }.filter { it > NUM }
 }
