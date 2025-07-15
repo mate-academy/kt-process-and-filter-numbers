b138a36e988d7e962bf04a1b25e3fbe5d17ae9a9
Dmytro Makhanok <dmmhnk23@gmail.com>
2025-07-15 12:30:57 +0200

    done

--- src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
+++ src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
@@ -1,5 +1,17 @@
 package mate.academy
 
-fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
+const val ZERO = 0
+const val TWO = 2
+const val MIN_VALUE = 25
 
+fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
+    var numList : MutableList<Int> = mutableListOf()
+    for (number in numbers) {
+        var value = when {
+            number % TWO == ZERO -> number / TWO
+            else -> number * 2
+        }
+        if (value > MIN_VALUE) numList.add(value)
+    }
+    return numList
 }
